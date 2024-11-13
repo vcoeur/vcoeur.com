fd63e929881e43511cac6dbaaf074152f3b4df04
Alice Voland <alice@vcoeur.com>
2024-11-13 08:55:35 +0100

    Simplify

--- deploy-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-poetry export > requirements.txt
-gcloud app deploy app-test.yaml --quiet
-#gcloud app deploy dispatch.yaml --quiet
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=$(dirname "$BASH_SOURCE")
-export PYTHONPATH=$PYTHONPATH:$BASEDIR
-exec /usr/bin/env gunicorn vcoeur.wsgi -b 0.0.0.0:8080 \
-  --preload --worker-class gthread --workers 4 --threads 4 \
-  --max-requests 1000 --max-requests-jitter 100 --timeout 30
