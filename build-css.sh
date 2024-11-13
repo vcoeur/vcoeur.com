676cd0cb55fd73d39679b0d26f6868d6b362c6b3
Alice Voland <alice@vcoeur.com>
2024-11-13 08:16:11 +0100

    Update CSS management

---
+++ build-css.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sass files/custom.scss files/style.css
+cp files/style.css* static/
--- make-css.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sass css/custom.scss files/style.css
-cp files/style.css static/style.css
